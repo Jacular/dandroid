fdc2a37b2ab54c1cdeb0b45845e0981ec8591c55
Jacular <417750747@qq.com>
2021-04-11 02:26:37 +0800

    组件化搭建

---
+++ module-home/src/main/java/com/dandroid/module_home/ModelHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.dandroid.module_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModelHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_model_home)
+    }
+}
