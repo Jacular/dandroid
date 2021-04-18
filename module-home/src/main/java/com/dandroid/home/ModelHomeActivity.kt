a93d067e7515b5c53e794704fad5e7f887453adb
Jacular <417750747@qq.com>
2021-04-18 18:36:46 +0800

    组件化搭建

--- module-home/src/main/java/com/dandroid/home/ModelHomeActivity.kt
+++ module-home/src/main/java/com/dandroid/home/ModelHomeActivity.kt
@@ -4,14 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dandroid.home.request.ApiService
 import com.dandroid.module_home.R
-import com.dandroid.network.NetworkApi
+//import com.dandroid.network.NetworkApi
 
 class ModelHomeActivity : AppCompatActivity() {
 
     //双重校验锁式-单例
-    val apiService: ApiService by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
-        NetworkApi.INSTANCE.getApi(ApiService::class.java, Build)
-    }
+//    val apiService: ApiService by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
+//        NetworkApi.INSTANCE.getApi(ApiService::class.java, BuildConfig.DEBUG)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
